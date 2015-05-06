fe34f367b771a259f237d1b5d13ae097a1710ee0
Luke Pawlik <lukepawlik@gmail.com>
2015-05-06 14:03:55 +0200

    Readme typo

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,17 +2,5 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
-
-  validates :title, presence: true
-  validates :price, presence: true, format: {:with => /\A\d+\.?\d{0,2}\z/, message: "Only two decimal places."}
-  validates :description, presence: true
-
-  def average_rating
-    if self.reviews.size > 0
-        self.reviews.average(:rating)
-    else
-        'undefined'
-    end
-  end
 end
 
