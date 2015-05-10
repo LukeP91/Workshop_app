c54f4254ce98bc71748a79e65fe7e48353a47082
Luke Pawlik <lukepawlik@gmail.com>
2015-05-10 16:57:17 +0200

    Add Sign up/Sign in/Logout links in navbar. Hide actions not allowed for particular users.

--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20140606091435) do
     t.string   "firstname"
     t.string   "lastname"
     t.boolean  "admin",                  default: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
